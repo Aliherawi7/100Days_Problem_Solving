4fecfb545953101a9680b0e260185d8a6bcfd932
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-20 23:27:20 +0430

    first google question soolved

--- src/topGoogleQuestions/number1/TwoSum.java
+++ src/topGoogleQuestions/number1/TwoSum.java
@@ -1,8 +1,30 @@
 package topGoogleQuestions.number1;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
+    public static void main(String[] args) {
+        int[] Given_nums = {2, 7, 11, 15};
+        int target = 9;
 
-    public
+        int[] result = twoSum(Given_nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+
+    public static int[] twoSum(int[] array, int target){
+        int[] indexArray = {-1, -1};
+
+        for(int i =0; i < array.length-1; i++){
+            int sum = array[i] + array[i+1];
+            if(sum == target){
+                indexArray[0] = i;
+                indexArray[1] = i+1;
+            }
+        }
+
+        return indexArray;
+    }
 
 }
