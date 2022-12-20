50eaa59dfdaa361fd4102b8bbde7e6bedc9f2afb
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-20 23:54:59 +0430

    method tested successfully

--- src/topGoogleQuestions/number2/LongestSubstringWithoutRepeatingCharacters.java
+++ src/topGoogleQuestions/number2/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,23 +1,28 @@
 package topGoogleQuestions.number2;
 
 public class LongestSubstringWithoutRepeatingCharacters {
-
+    public static void main(String[] args) {
+        String input =  "getLongestSubstringWithoutRepeatingChar";
+        int result = getLongestSubstringWithoutRepeatingChar(input);
+        System.out.println(result);
+    }
 
     public static int getLongestSubstringWithoutRepeatingChar(String text){
+        text = text.toLowerCase();
         StringBuilder subString = new StringBuilder();
         String subHolder = "";
 
-        for(int i=0; i < text.length(); i++ ){
+        for(int i= 0; i < text.length(); i++ ){
 
             if(subString.toString().contains(text.charAt(i)+"")){
                 if(subHolder.length() < subString.length()){
                     subHolder = subString.toString();
                 }
-                subString.delete(0, subString.length()-1);
+                subString = new StringBuilder();
             }
             subString.append(text.charAt(i));
         }
-
-        return subString.length();
+        System.out.println(subHolder);
+        return subHolder.length();
     }
 }
