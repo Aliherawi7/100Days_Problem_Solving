6218c532a1ccea82778694788024666c9f39b312
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-20 23:45:42 +0430

    method implemented

--- src/topGoogleQuestions/number2/LongestSubstringWithoutRepeatingCharacters.java
+++ src/topGoogleQuestions/number2/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,23 @@
 package topGoogleQuestions.number2;
 
 public class LongestSubstringWithoutRepeatingCharacters {
+
+
+    public static int getLongestSubstringWithoutRepeatingChar(String text){
+        StringBuilder subString = new StringBuilder();
+        String subHolder = "";
+
+        for(int i=0; i < text.length(); i++ ){
+
+            if(subString.toString().contains(text.charAt(i)+"")){
+                if(subHolder.length() < subString.length()){
+                    subHolder = subString.toString();
+                }
+                subString.delete(0, subString.length()-1);
+            }
+            subString.append(text.charAt(i));
+        }
+
+        return subString.length();
+    }
 }
