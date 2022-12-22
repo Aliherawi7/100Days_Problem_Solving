2db35109c051d1bdec7328bf16472dff3849c026
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-22 16:54:27 +0430

    problem number 4 solved

--- src/topGoogleQuestions/number4/LongestPalindromicSubstring.java
+++ src/topGoogleQuestions/number4/LongestPalindromicSubstring.java
@@ -1,4 +1,41 @@
 package topGoogleQuestions.number4;
 
 public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String test = "ghuiubabababd987utu";
+        System.out.println(test);
+        System.out.println(getLongestPalindromicSubstring(test));
+    }
+
+
+    public static String getLongestPalindromicSubstring(String str){
+        String subString = "";
+
+        for (int i=0; i<str.length(); i++){
+            StringBuilder holder = new StringBuilder();
+            // start adding the chars to the holder from i
+            int counter = i;
+            while(counter <= str.lastIndexOf(str.charAt(i))){
+                holder.append(str.charAt(counter));
+                counter++;
+            }
+
+            // check if the holder value is prime
+            boolean isPrime = true;
+            int start =0, end = holder.length()-1;
+            while(start < end){
+                if(holder.charAt(start) != holder.charAt(end)){
+                    isPrime = false;
+                }
+                start++;
+                end--;
+            }
+
+            // if the holder value is prime and its size is bigger than the subString
+            if(isPrime && holder.length() > subString.length()){
+                subString = holder.toString();
+            }
+        }
+        return subString;
+    }
 }
