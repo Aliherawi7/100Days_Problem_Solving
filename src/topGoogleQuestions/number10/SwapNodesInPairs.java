05e941c084e51c60a6cbf2da948509fbd10878ff
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-23 22:59:23 +0430

    swap method implemented. question solved

--- src/topGoogleQuestions/number10/SwapNodesInPairs.java
+++ src/topGoogleQuestions/number10/SwapNodesInPairs.java
@@ -1,4 +1,29 @@
 package topGoogleQuestions.number10;
 
+import topGoogleQuestions.number8.LinkList;
+import topGoogleQuestions.number8.Node;
+
 public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        LinkList list = new LinkList();
+        list.addValue(1);
+        list.addValue(2);
+        list.addValue(3);
+        list.addValue(4);
+        list.displayList();
+
+        System.out.println();
+        System.out.println("---- after swapping the numbers ----");
+        swapNodesInPairs(list);
+        list.displayList();
+    }
+    public static void swapNodesInPairs(LinkList linkList){
+        Node current = linkList.getNodes();
+        while (current != null){
+            int holder = current.value;
+            current.value = current.next.value;
+            current.next.value = holder;
+            current = current.next.next;
+        }
+    }
 }
