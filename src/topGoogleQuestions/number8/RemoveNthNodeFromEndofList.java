6bc5d9e93e720dc28eba6aac783d78564bcb6091
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-23 18:20:30 +0430

    removeNthNodeFromEndOfList method implemented. question sovled

--- src/topGoogleQuestions/number8/RemoveNthNodeFromEndofList.java
+++ src/topGoogleQuestions/number8/RemoveNthNodeFromEndofList.java
@@ -1,4 +1,38 @@
 package topGoogleQuestions.number8;
 
 public class RemoveNthNodeFromEndofList {
+    public static void main(String[] args) {
+        LinkList myList = new LinkList();
+        myList.addValue(1);
+        myList.addValue(2);
+        myList.addValue(3);
+        myList.addValue(4);
+        myList.addValue(5);
+        myList.displayList();
+        System.out.println();
+        System.out.println("---- after deleting the nth node -----");
+        removeNthNodeFromEndofList(myList, 2);
+        myList.displayList();
+    }
+
+    public static void removeNthNodeFromEndofList(LinkList linkList, int nthNode){
+        Node node = linkList.getNodes();
+        if(nthNode == 1){
+            linkList.removeFirst();
+            return;
+        }
+        int counter = 2;
+        Node current = node;
+        while(current != null){
+            if(counter == nthNode){
+                current.next = current.next.next;
+                break;
+            }
+            counter++;
+            current = current.next;
+        }
+    }
 }
+
+
+
