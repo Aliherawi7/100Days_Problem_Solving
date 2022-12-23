79ef48c361306626ed2b5de3dcef77a2e9a17c78
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-23 18:18:42 +0430

    simple linklist created for test in RemoveNthNodeFromEndOfList class

--- src/topGoogleQuestions/number8/LinkList.java
+++ src/topGoogleQuestions/number8/LinkList.java
@@ -1,4 +1,54 @@
 package topGoogleQuestions.number8;
 
 public class LinkList {
+    private Node first;
+    public LinkList(){
+        first = null;
+    }
+
+    public void addValue(int value){
+        Node node = new Node(value);
+        node.next = first;
+        first = node;
+    }
+
+    public int removeFirst(){
+        int temp = first.value;
+        first = first.next;
+        return temp;
+    }
+
+    public Node getNodes(){
+        return first;
+    }
+
+    public void displayList(){
+        Node current = first;
+        while(current != null) {
+            current.displayNode();
+            current = current.next;
+        }
+    }
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+
+
+
+
+
+
+
+}
+class Node{
+    int value;
+    Node next;
+    public Node(int value){
+        this.value = value;
+    }
+
+    public void displayNode(){
+        System.out.print(value + " ");
+    }
 }
