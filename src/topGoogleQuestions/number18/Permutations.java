80f76a60d3de3cf89eda9fcaaa6de2499a4ecd39
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-25 22:21:37 +0430

    findFirstMissingPositive method declered

--- src/topGoogleQuestions/number18/Permutations.java
+++ src/topGoogleQuestions/number18/Permutations.java
@@ -1,4 +1,17 @@
 package topGoogleQuestions.number18;
 
 public class Permutations {
+
+
+    public static int[][] getPermutations(int[] numbers){
+        int[][] perm = new int[numbers.length * 2][numbers.length];
+
+        for(int i = 0; i < perm.length; i++){
+            for(int j = 0; j < numbers.length; j++){
+
+            }
+        }
+
+        return perm;
+    }
 }
