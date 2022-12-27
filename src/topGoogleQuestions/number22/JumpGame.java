a8c5557a509b19fc284afb73c9b7282a1666dce4
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-27 17:29:07 +0430

    jump method implemented. question solved

--- src/topGoogleQuestions/number22/JumpGame.java
+++ src/topGoogleQuestions/number22/JumpGame.java
@@ -1,4 +1,27 @@
 package topGoogleQuestions.number22;
 
 public class JumpGame {
+    public static void main(String[] args) {
+        int[] input1 = {2,3,1,1,4};
+        boolean output1 = jumpGame(input1);
+        System.out.println(output1);
+        int[] input2 = {3,2,1,0,4};
+        boolean output2 = jumpGame(input2);
+        System.out.println(output2);
+    }
+
+    public static boolean jumpGame(int[] numbers){
+        if(numbers.length <=1) return false;
+
+        int index = 0;
+        while(index < numbers.length){
+            // if the element is
+            if(numbers[index] <= 0){
+                return false;
+            }
+            // jump from index to index + numbers[index]
+            index += numbers[index];
+        }
+        return true;
+    }
 }
