4635d3c0356b5c871b9fed34087f737052407e10
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-28 03:10:52 +0430

    comment added for codes description

--- src/topGoogleQuestions/number27/RemoveConsecutiveDublicates.java
+++ src/topGoogleQuestions/number27/RemoveConsecutiveDublicates.java
@@ -8,11 +8,15 @@ public class RemoveConsecutiveDublicates {
     }
 
     public static String removeConsecutiveDublicates(String input){
-
+        // create a empty stringBuilder to append each single character from the input string
         StringBuilder output = new StringBuilder();
-
+        // iterate over the input string
         for(int i =0; i< input.length(); i++){
+            // append the current character at i index from input string
             output.append(input.charAt(i));
+
+            // if there are any consecutive duplicate character in the
+            // next index, then increment the i index to the new not duplicated character's index
             while(i < input.length()-1 && input.charAt(i) == input.charAt(i+1)){
                 i++;
             }
