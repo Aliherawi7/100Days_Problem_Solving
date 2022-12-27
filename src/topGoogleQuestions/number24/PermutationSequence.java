5c7f03fa0637d0e74941ba41cf745bdebe792a95
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-28 00:21:38 +0430

    question solved

--- src/topGoogleQuestions/number24/PermutationSequence.java
+++ src/topGoogleQuestions/number24/PermutationSequence.java
@@ -1,4 +1,38 @@
 package topGoogleQuestions.number24;
 
 public class PermutationSequence {
+    public static void main(String[] args) {
+        String num = permutationSequence(4, 9);
+        System.out.println(num);
+    }
+
+
+    public static String permutationSequence(int n, int k){
+        StringBuilder sb = new StringBuilder();
+        StringBuilder numbers = new StringBuilder();
+        for(int i = 1; i <= n; i++)
+            numbers.append(i);
+        permute(numbers.toString(), 0, numbers.length()-1, sb);
+        return sb.toString().split(" ")[k];
+    }
+    public static void permute(String str, int l , int r, StringBuilder sb){
+        if(l == r){
+            sb.append(" ").append(str);
+        }else {
+            for(int i = l; i <= r; i++){
+                str = swap(str, l, i);
+                permute(str, l + 1, r, sb);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    private static String swap(String str, int a, int b) {
+        char temp;
+        char[] charArray = str.toCharArray();
+        temp = charArray[a];
+        charArray[a] = charArray[b];
+        charArray[b] = temp;
+        return String.valueOf(charArray);
+    }
 }
