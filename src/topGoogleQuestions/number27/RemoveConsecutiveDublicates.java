521232bccd46d2f52b6da53f66aa0c2c4a9a6afa
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-28 15:14:45 +0430

    solution number 2 also added

--- src/topGoogleQuestions/number27/RemoveConsecutiveDublicates.java
+++ src/topGoogleQuestions/number27/RemoveConsecutiveDublicates.java
@@ -2,12 +2,19 @@ package topGoogleQuestions.number27;
 
 public class RemoveConsecutiveDublicates {
     public static void main(String[] args) {
-        String input = "hhhhhhheellllllloooo";
-        String output = removeConsecutiveDublicates(input);
-        System.out.println(output);
+        // solution 1
+        String input1 = "hhhhhhheellllllloooo";
+        String output1 = removeConsecutiveDuplicates(input1);
+        System.out.println(output1);
+
+        // solution 2
+        String input2 = "hhhhhhheellllllloooo";
+        String output2 = removeConsecutiveDup(input2);
+        System.out.println(output2);
     }
 
-    public static String removeConsecutiveDublicates(String input){
+    // solution 1
+    public static String removeConsecutiveDuplicates(String input){
         // create a empty stringBuilder to append each single character from the input string
         StringBuilder output = new StringBuilder();
         // iterate over the input string
@@ -19,6 +26,25 @@ public class RemoveConsecutiveDublicates {
             // next index, then increment the i index to the new not duplicated character's index
             while(i < input.length()-1 && input.charAt(i) == input.charAt(i+1)){
                 i++;
+            }
+        }
+        return output.toString();
+    }
+
+    // solution 2
+    public static String removeConsecutiveDup(String input){
+        // create an empty StringBuilder object
+        StringBuilder output = new StringBuilder();
+        // append the first letter of the input string
+        output.append(input.charAt(0));
+
+        // iterate over the input string
+        for(int i = 1; i < input.length(); i++){
+
+            // check if the the current character from input string is not equal to
+            // the last character of output string, then append it to output
+            if(input.charAt(i) != output.charAt(output.length()-1)){
+                output.append(input.charAt(i));
             }
         }
         return output.toString();
