93ebd8c9b17561dbcac333090916fed32b177b79
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-30 23:33:01 +0430

    question solved

--- src/topGoogleQuestions/number33/ReverseNumber.java
+++ src/topGoogleQuestions/number33/ReverseNumber.java
@@ -1,4 +1,25 @@
 package topGoogleQuestions.number33;
 
 public class ReverseNumber {
+    public static void main(String[] args) {
+        int input = 12345;
+        int output = reverseNumber(input);
+        System.out.println(output);
+    }
+
+    public static int reverseNumber(int number){
+        int holder = 0;
+        // the loop will iterate while the number is not zero if the number is zero it will stop
+        while(number != 0){
+            // get the last digit of the input number
+            int remainder = number % 10;
+            // remove the last digit from input number
+            number = number/10;
+            // add a zero at the end of the holder
+            holder = holder * 10;
+            // add the remainder digit at the end of the holder
+            holder += remainder;
+        }
+        return holder;
+    }
 }
---
+++ src/topGoogleQuestions/number33/readme.txt
@@ -0,0 +1,15 @@
+
+
+### 33. Reverse Number
+
+description:
+write a function takes a number and reverse it.
+for example the input number is 12345  and the output is 54321
+
+example 1:
+input : 45632
+output: 23654
+
+example 2:
+input : 940321
+output: 123049
