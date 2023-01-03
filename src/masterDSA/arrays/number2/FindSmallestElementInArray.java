f0e42c67362910c4c76d234145f2d426865f2775
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-03 16:39:55 +0430

    question number 2 of arrays packaged solved

--- src/masterDSA/arrays/number2/FindSmallestElementInArray.java
+++ src/masterDSA/arrays/number2/FindSmallestElementInArray.java
@@ -1,4 +1,17 @@
-package masterDSA.arrays.number1.number2;
+package masterDSA.arrays.number2;
 
 public class FindSmallestElementInArray {
+    public static void main(String[] args) {
+        int[] input = {4,7,3,7,2,1,-3,2};
+        int output = findSmallestElement(input);
+        System.out.println(output);
+    }
+    public static int findSmallestElement(int[] numbers){
+        int min = 0;
+        for (int number : numbers) {
+            if (min > number)
+                min = number;
+        }
+        return min;
+    }
 }
