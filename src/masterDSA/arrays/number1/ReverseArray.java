947a128d8a805d3f9b4805cdc63cf16e4cfb98a9
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-03 16:29:17 +0430

    question number 1 in array package solved

--- src/masterDSA/arrays/number1/ReverseArray.java
+++ src/masterDSA/arrays/number1/ReverseArray.java
@@ -1,4 +1,22 @@
 package masterDSA.arrays.number1;
 
+import java.util.Arrays;
+
 public class ReverseArray {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        reverseArray(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+    public static void reverseArray(int[] numbers){
+        int tail = 0,  head = numbers.length - 1;
+
+        while(tail <= head){
+            int temp = numbers[tail];
+            numbers[tail] = numbers[head];
+            numbers[head] = temp;
+            tail++;
+            head--;
+        }
+    }
 }
