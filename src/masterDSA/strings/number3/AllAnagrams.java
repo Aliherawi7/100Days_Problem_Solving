dda6f6f705513b00efb2d99af61a85a5fa8cd575
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-06 00:10:45 +0430

    question number 3 from string package solved

--- src/masterDSA/strings/number3/AllAnagrams.java
+++ src/masterDSA/strings/number3/AllAnagrams.java
@@ -1,4 +1,20 @@
 package masterDSA.strings.number3;
 
-public class StringRotation {
+import java.util.Arrays;
+
+public class AllAnagrams {
+    public static void main(String[] args) {
+        String p = "hello";
+        String s = "world";
+        String[] output = getAllAnagrams(p, s);
+        System.out.println(Arrays.toString(output));
+    }
+    public static String[] getAllAnagrams(String p , String s){
+        String[] anagrams = new String[p.length()];
+        for(int i = 0; i < p.length(); i++){
+            String holder = p.charAt(i) + s;
+            anagrams[i] = holder;
+        }
+        return anagrams;
+    }
 }
