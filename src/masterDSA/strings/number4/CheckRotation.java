7da19ca199f9293890835c45e6f8ca65e39f8a4e
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-06 11:41:46 +0430

    question number 4 solved by concatenating method solution

--- src/masterDSA/strings/number4/CheckRotation.java
+++ src/masterDSA/strings/number4/CheckRotation.java
@@ -1,4 +1,24 @@
 package masterDSA.strings.number4;
 
 public class CheckRotation {
+    public static void main(String[] args) {
+        String input = "abcd";
+        String target = "bdca";
+        System.out.println(checkRotation(input, target));
+    }
+
+    /* in this solution, I used concatenating solution, the way is to creating another string by concatenating first string with itself.
+     and then check if the target string is substring of concatenated string
+     for example: "JavaProgrammer" => after concatenated => "JavaProgrammerJavaProgrammer"
+     now it contains every possible rotation of first string
+     */
+
+    public static boolean checkRotation(String str, String target){
+        if(str.length() != target.length()) return false;
+
+        str = str.concat(str);
+        return str.contains(target);
+    }
+
+
 }
