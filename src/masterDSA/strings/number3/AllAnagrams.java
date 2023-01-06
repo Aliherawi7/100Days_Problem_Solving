d4bd93dc695c70559baa499ac43a723103fdc3a3
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-06 11:13:10 +0430

    question number 3 from string package revised

--- src/masterDSA/strings/number3/AllAnagrams.java
+++ src/masterDSA/strings/number3/AllAnagrams.java
@@ -1,20 +1,40 @@
 package masterDSA.strings.number3;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class AllAnagrams {
     public static void main(String[] args) {
-        String p = "hello";
-        String s = "world";
-        String[] output = getAllAnagrams(p, s);
-        System.out.println(Arrays.toString(output));
+        String p = "ab";
+        String s = "abxeba";
+        System.out.println(getAllAnagrams(p, s));
     }
-    public static String[] getAllAnagrams(String p , String s){
-        String[] anagrams = new String[p.length()];
-        for(int i = 0; i < p.length(); i++){
-            String holder = p.charAt(i) + s;
-            anagrams[i] = holder;
+    public static ArrayList<Integer> getAllAnagrams(String p , String s){
+        ArrayList<Integer> indices = new ArrayList<>();
+        String holder = s;
+        int index = 0;
+        // find all indices that contains string p
+        while(index <= s.length()){
+            if(holder.contains(p)){
+                index = holder.indexOf(p);
+                indices.add(index);
+                holder = s.substring(index+p.length());
+            }else{
+                break;
+            }
         }
-        return anagrams;
+        // find all index that contains the reverse of string p
+        holder = s;
+        p = new StringBuilder(p).reverse().toString();
+        index = 0;
+        while(index <= s.length()){
+            if(holder.contains(p)){
+                index = holder.indexOf(p);
+                indices.add(index);
+                holder = s.substring(index+p.length());
+            }else {
+                break;
+            }
+        }
+        return indices;
     }
 }
--- src/masterDSA/strings/number3/readme.txt
+++ src/masterDSA/strings/number3/readme.txt
@@ -8,5 +8,5 @@ of all the start indices of p's anagrams in s.
 
 
 Example 1:
-input : p = "hello", s = "world"
-output : ["hworld", "eworld", "lworld", "lworld", "oworld"]
+input : p = "ab", s = "abxeba"
+output : [0, 4]
