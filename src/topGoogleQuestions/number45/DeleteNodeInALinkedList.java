972c70c6e922ae1b2242d35967facd272148f484
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-11 19:00:35 +0430

    deleteNodeInALinkedList method implemented. question 45 solved

--- src/topGoogleQuestions/number45/DeleteNodeInALinkedList.java
+++ src/topGoogleQuestions/number45/DeleteNodeInALinkedList.java
@@ -1,4 +1,29 @@
 package topGoogleQuestions.number45;
 
+import topGoogleQuestions.number8.LinkList;
+import topGoogleQuestions.number8.Node;
+
 public class DeleteNodeInALinkedList {
+    public static void main(String[] args) {
+        LinkList list = new LinkList();
+        list.addValue(9);
+        list.addValue(1);
+        list.addValue(5);
+        list.addValue(4);
+        int value = 5;
+        deleteNodeInLinkedList(list, value);
+        list.displayList();
+    }
+    public static void deleteNodeInLinkedList(LinkList list, int value){
+        Node nodes = list.getNodes();
+        while(nodes != null){
+            if(nodes.next.value == value){
+                nodes.next = nodes.next.next;
+                break;
+            }
+            nodes = nodes.next;
+        }
+
+    }
+
 }
