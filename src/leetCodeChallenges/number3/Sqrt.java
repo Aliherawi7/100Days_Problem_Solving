c371a016ce10650b3f63d45fbeeafed18e929a00
Aliherawi7 <Aliherawi7@gmail.com>
2023-01-20 01:54:42 +0430

    sqrt(x) method implemented. question number 3 solved

--- src/leetCodeChallenges/number3/Sqrt.java
+++ src/leetCodeChallenges/number3/Sqrt.java
@@ -1,4 +1,27 @@
-package leetCodeChallenges.number;
+package leetCodeChallenges.number3;
 
 public class Sqrt {
+    public static void main(String[] args) {
+        System.out.println(sqrt(1));
+    }
+
+    public static int sqrt(int x){
+        if(x == 1 || x == 0) return x;
+
+        int low = 0, high = x;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int sqrt = mid * mid;
+            System.out.println("sqrt " + sqrt);
+            if(sqrt == x){
+                System.out.println("mid " + mid);
+                return mid;
+            }else if(sqrt > x){
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
 }
