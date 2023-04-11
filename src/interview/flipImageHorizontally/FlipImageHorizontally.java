0b5ae71d33a2aa100f8e13559d528bfe57fe54a5
ali herawi <aliherawi7@gmail.com>
2023-04-11 08:46:45 +0430

    question descriptions added and solved

--- src/interview/flipImageHorizontally/FlipImageHorizontally.java
+++ src/interview/flipImageHorizontally/FlipImageHorizontally.java
@@ -1,4 +1,36 @@
 package interview.flipImageHorizontally;
 
+import java.util.Arrays;
+
 public class FlipImageHorizontally {
+
+    public static void main(String[] args) {
+        int[][] images = {{1, 1, 0}, {1, 0, 1}, {0, 0, 0}};
+        int[][] image = flipHorizontally(images);
+        for(int[] img : image){
+            System.out.println(Arrays.toString(img));
+        }
+    }
+
+    public static int[][] flipHorizontally(int[][] image) {
+        for (int i = 0; i < image.length; i++) {
+            int start = 0;
+            int end = image[i].length - 1;
+
+            // this loop is reversing the row
+            while (start != end) {
+                int holder = image[i][start];
+                image[i][start] = image[i][end];
+                image[i][end] = holder;
+                start++;
+                end--;
+            }
+            // this for is inverting the row
+            for(int j = 0; j < image[i].length; j++){
+                image[i][j] = image[i][j] == 1 ? 0:1;
+            }
+
+        }
+        return image;
+    }
 }
---
+++ src/interview/flipImageHorizontally/readme.txt
@@ -0,0 +1,15 @@
+
+### Flip the image Horizontally
+
+descriptions:
+Given an n x n binary images, flip the image horizontally then invert and
+return the resulting image
+To flip an image horizontally means that each row of the image is reversed.
+for example, flipping  [1, 1, 0] horizontally results in [0, 1, 1]
+To invert an image means that each 0 is replaced by 1 and  each 1 is replaced by 0
+for example inverting [0, 1, 1] results in [1, 0, 0]
+
+ex:
+input : {[1,1,0], [1,0,1], [0,0,0]}
+output:{[1,0,0],[0,1,0],[1,1,1,]}
+
