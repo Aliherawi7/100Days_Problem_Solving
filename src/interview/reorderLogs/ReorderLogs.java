b5fb2206b37d7a0993b167b4cde816d8d20b5382
ali herawi <aliherawi7@gmail.com>
2023-04-12 09:17:05 +0430

    reorderlogs class renamed

--- src/interview/reorderLogs/ReorderLogs.java
+++ src/interview/reorderLogs/ReorderLogs.java
@@ -1,5 +1,6 @@
 package interview.reorderLogs;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,9 +11,7 @@ public class ReorderLogs {
         String[] logs = { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"};
         logsReorder(logs);
         logs = logsReorder(logs);
-        for(String str : logs){
-            System.out.println(str);
-        }
+        System.out.println(Arrays.toString(logs));
     }
 
     public static String[] logsReorder(String[] logs){
@@ -20,6 +19,16 @@ public class ReorderLogs {
         Map<String, Integer> numMap = new LinkedHashMap<>();
         String[] reordered = new String[logs.length];
 
+
+        /*
+        * iterate over the logs array and each iteration
+        * we split the current element with " " space and then
+        * concatenate them together from index 1 in a string var called holder
+        * then if the holder contains string we added the indentifier at the end
+        * of holder var and then put it in the stringMap as key and corresponding i index as
+        * value. the same story also if holder contains integer but we add it in the
+        * numMap
+        * */
         for(int i = 0; i < logs.length; i++){
             String[] str = logs[i].split(" ");
             StringBuilder holder = new StringBuilder();
@@ -34,6 +43,14 @@ public class ReorderLogs {
                 stringMap.put(holder.toString(), i);
             }
         }
+
+        /*
+        * in here now we get the stringMap all keys and then sort them
+        * and then for each one of the keys we get the value and because the value
+        * is the log index in the original logs array so we add the logs from
+        * logs array into the reordered array with regards of their order in the
+        * stringMap
+        * */
         AtomicInteger counter = new AtomicInteger();
         stringMap
                 .keySet()
@@ -42,6 +59,9 @@ public class ReorderLogs {
                 .forEach(item -> {
                     reordered[counter.getAndIncrement()] = logs[stringMap.get(item)];
                 });
+        /*
+        * in here we set the atomic integer to the
+        * */
         counter.set(logs.length-numMap.size());
         numMap.keySet().forEach(item -> {
             reordered[counter.getAndIncrement()] = logs[numMap.get(item)];
--- src/interview/reorderLogs/readme.txt
+++ src/interview/reorderLogs/readme.txt
@@ -19,5 +19,4 @@ return the final order of logs
 Ex 1:
 input:
 logs: [ "dig1 8 1 5 1", "let1 art can, "dig2 3 6", "let2 own kit dig", "let3 art zero"]
-
 output: ["let1 rt can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6" ]
